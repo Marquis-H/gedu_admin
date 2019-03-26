af6893a29702725f3102dc5eb078452de5e28ad8
Marquis <511680713@qq.com>
2019-03-26 11:50:03 +0800

    [update]word type command

--- src/Command/WordTabsUpdateCommand.php
+++ src/Command/WordTabsUpdateCommand.php
@@ -10,6 +10,7 @@ namespace Admin\Command;
 
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -21,7 +22,8 @@ class WordTabsUpdateCommand extends ContainerAwareCommand
 	protected function configure()
 	{
 		$this
-			->setName('word:update:tabs');
+			->setName('word:update:tabs')
+			->addArgument('tab', InputArgument::REQUIRED, 'tab name');
 	}
 
 	/**
@@ -32,17 +34,18 @@ class WordTabsUpdateCommand extends ContainerAwareCommand
 		$dataPath = $this->getContainer()->get('kernel')->getRootDir() . '/../data/';
 		$row = 1;
 		$conn = $this->getContainer()->get('doctrine.dbal.default_connection');
-		if (($handle = fopen($dataPath . "six.csv", "r")) !== FALSE) {
+		$tab = $input->getArgument('tab');
+		if ($tab && ($handle = fopen($dataPath . $tab . ".csv", "r")) !== FALSE) {
 			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 				$row++;
 				$word = $data[0];
 				try {
 					$words = $conn->fetchAll('select * from word where word = \'' . $word . '\'');
-					if($words){
+					if ($words) {
 						$tabs = $words[0]['tabs'];
 						$tabs = unserialize($tabs);
-						if (!in_array('six', $tabs)) {
-							array_push($tabs, 'six');
+						if (!in_array($tab, $tabs)) {
+							array_push($tabs, $tab);
 							$conn->update('word', [
 								'tabs' => serialize($tabs),
 							], ['word' => $word]);
